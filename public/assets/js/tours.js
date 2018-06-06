d3f9118311bacd7a90c507f5361e7cbc250408c6
AFlak <AndrewMFlak@gmail.com>
2018-06-06 17:25:37 -0400

    including table link

--- public/assets/js/tours.js
+++ public/assets/js/tours.js
@@ -9,9 +9,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var tourInfo = [{
-    name: "test",
-    desc: "test description",
-    img: "https://pbs.twimg.com/profile_images/575317158688768000/BWZD2Spg_400x400.png"
 }
 ];
 console.log(tourInfo);
